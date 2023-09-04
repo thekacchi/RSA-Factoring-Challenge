a97e8d205c9ec913cabbac2a4ed540c2135cf48f
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-03 20:10:54 -0700

    oh God

--- factors.c
+++ factors.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
 void factorize(int n)
 {
 	int factor = 2;
@@ -26,7 +26,31 @@ void factorize(int n)
 
 	printf("\n");
 }
+*/
 
+void factorize(int n)
+{
+	if (n <= 1)
+	{
+		printf("%d=%d*%d\n", n, n, 1);
+		return;
+	}
+
+	int factor1 = 1;
+	int factor2 = n;
+
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			factor1 = i;
+			factor2 = n / i;
+			break;
+		}
+	}
+
+	printf("%d=%d*%d\n", n, factor2, factor1);
+}
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
